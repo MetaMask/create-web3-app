4bc5818fe1c4469f02bfa05a0b46a21b02f5fe55
Alejandro <smarketing.cuz@gmail.com>
2025-04-28 12:03:28 +0200

    Comment out the React Web3 Starter template in templates.ts for future reference.

--- src/constants/templates.ts
+++ src/constants/templates.ts
@@ -4,12 +4,12 @@ export const TEMPLATES = [
     id: "next-sdk-quickstart",
     degitSource: "MetaMask/metamask-sdk-examples/examples/quickstart",
   },
-  {
-    name: "React Web3 Starter",
-    id: "react-web3-starter",
-    repo_url: "https://github.com/Consensys/react-web3-starter.git",
-    packageName: "@consensys/react-web3-starter",
-  },
+  // {
+  //   name: "React Web3 Starter",
+  //   id: "react-web3-starter",
+  //   repo_url: "https://github.com/Consensys/react-web3-starter.git",
+  //   packageName: "@consensys/react-web3-starter",
+  // },
   {
     name: "MetaMask <-> Dynamic Quickstart",
     id: "metamask-dynamic",
